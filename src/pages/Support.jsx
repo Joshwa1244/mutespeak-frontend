1d29aa982bf9cec49135506edc139e5529f32bab
Joshwa <joshan1244@gmail.com>
2026-08-04 18:10:24 +0530

    Supprt Page Updation

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { DodoPayments } from "dodopayments-checkout";
-import { paymentService } from "./paymentService";
+import { paymentService } from "../services/paymentService";
 import "./Support.css";
 
 // -------------------------------------------------------------
